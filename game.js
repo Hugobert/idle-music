047ad3f8274fac263a28c1edce7f9e37e8d803d3
Hugo <12248563+Hugobert@users.noreply.github.com>
2018-08-04 17:55:13 +0200

    Added money and thoughts. Changed currency of weed to money

--- game.js
+++ game.js
@@ -1,10 +1,18 @@
 globalCostMultiplier = 1;
 tickrate = 50;
 
+money = {
+  nameSingular: "€",
+  namePlural: "€",
+  amount: new Decimal(0),
+}
+
 creativity = {
   nameSingular: "Creativity",
   namePlural: "Creativity",
-  amount: new Decimal(0)
+  amount: new Decimal(0),
+  value: 0.1,
+  gainPerSec: new Decimal(0)
 }
 
 notes = {
@@ -25,13 +33,24 @@ bars = {
   currency: notes
 }
 
-weed = {
-  nameSingular: "Weed",
-  namePlural: "Weed",
+thoughts = {
+  nameSingular: "Thought",
+  namePlural: "Thoughts",
   amount: new Decimal(0),
   cost: 1,
   costMultiplier: 1.1,
   currency: bars,
+  outputType: creativity,
+  outputAmount: 0.1
+}
+
+weed = {
+  nameSingular: "Weed",
+  namePlural: "Weed",
+  amount: new Decimal(0),
+  cost: 8,
+  costMultiplier: 1.1,
+  currency: money,
   outputType: creativity,
   outputAmount: 1
 }
@@ -61,6 +80,13 @@ document.getElementById("addBar").addEventListener("click", function(){
   updateValues();
 })
 
+document.getElementById("addThought").addEventListener("click", function(){
+  add(thoughts, 1);
+  subtract(thoughts.currency, thoughts.cost);
+  thoughts.cost = thoughts.cost * thoughts.costMultiplier;
+  updateValues();
+})
+
 document.getElementById("addWeed").addEventListener("click", function(){
   add(weed, 1);
   subtract(weed.currency, weed.cost);
@@ -68,9 +94,17 @@ document.getElementById("addWeed").addEventListener("click", function(){
   updateValues();
 })
 
+document.getElementById("sellCreativity").addEventListener("click", function(){
+  add(money, creativity.value);
+  subtract(creativity, 1);
+  updateValues();
+})
+
 // Automation
 setInterval(function(){
     weed.outputType.amount = weed.outputType.amount.plus((weed.amount * weed.outputAmount)/(1000/tickrate));
+    thoughts.outputType.amount = thoughts.outputType.amount.plus((thoughts.amount * thoughts.outputAmount)/(1000/tickrate));
+    creativity.gainPerSec = (weed.outputAmount*weed.amount) + (thoughts.outputAmount*thoughts.amount);
     updateValues();
 }, tickrate);
 
@@ -78,15 +112,17 @@ setInterval(function(){
 function updateValues(){
 
   // Table
-  document.getElementById("creativityAmount").innerHTML = creativity.amount.toFixed(2);
+  document.getElementById("moneyAmount").innerHTML = money.amount.toFixed(2) + " " + money.namePlural;
+  document.getElementById("creativityAmount").innerHTML = creativity.amount.toFixed(2) + " (" + creativity.gainPerSec + "/s)";
   document.getElementById("notesAmount").innerHTML = notes.amount.toFixed(2);
   document.getElementById("barsAmount").innerHTML = bars.amount.toFixed(2);
-
+  document.getElementById("thoughtAmount").innerHTML = thoughts.amount.toFixed(0);
   document.getElementById("weedAmount").innerHTML = weed.amount.toFixed(0);
 
   // Button costs
   document.getElementById("noteCost").innerHTML = notes.cost.toFixed(2) + " " + notes.currency.namePlural;
   document.getElementById("barCost").innerHTML = bars.cost.toFixed(2) + " " + bars.currency.namePlural;
+  document.getElementById("thoughtCost").innerHTML = thoughts.cost.toFixed(2) + " " + thoughts.currency.namePlural;
   document.getElementById("weedCost").innerHTML = weed.cost.toFixed(2) + " " + weed.currency.namePlural;
 
   // Button availability
@@ -102,16 +138,28 @@ function updateValues(){
     document.getElementById("addBar").disabled = true;
   }
 
+  if(thoughts.currency.amount >= thoughts.cost){
+    document.getElementById("addThought").disabled = false;
+  } else {
+    document.getElementById("addThought").disabled = true;
+  }
+
   if(weed.currency.amount >= weed.cost){
     document.getElementById("addWeed").disabled = false;
   } else {
     document.getElementById("addWeed").disabled = true;
   }
+
+  if(creativity.amount >= 1){
+    document.getElementById("sellCreativity").disabled = false;
+  } else {
+    document.getElementById("sellCreativity").disabled = true;
+  }
 }
 
 function add(target, count){
   //target.amount = target.amount + count;
-  target.amount = target.amount.plus(1);
+  target.amount = target.amount.plus(count);
 }
 
 function subtract(target, count){
