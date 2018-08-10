e42603b158646d75d18e15d87b07e0503dd466da
Hugobert <12248563+Hugobert@users.noreply.github.com>
2018-08-10 13:53:23 +0200

    Changed some of the note-phrase-chain-song logic

--- js/items.js
+++ js/items.js
@@ -1,5 +1,5 @@
 newGame = {
-  "version": "0.0.2 a",
+  "version": "0.0.2 a0.2",
   "tickspeed": 100,
 
   "money": {
@@ -22,9 +22,11 @@ newGame = {
     "name": "Phrases",
     "amount": new Decimal(0),
     "gainPerSec": new Decimal(0),
-    "cost": 16,
-    "buyCurrency": "notes",
-    "isPartOf": "chains"
+    "cost": 160,
+    "buyCurrency": "creativity",
+    "isPartOf": "chains",
+    "buys": 16,
+    "requiresLower": 16
   },
 
   "chains": {
@@ -33,7 +35,9 @@ newGame = {
     "gainPerSec": new Decimal(0),
     "cost": 16,
     "buyCurrency": "phrases",
-    "isPartOf": "songs"
+    "isPartOf": "songs",
+    "buys": 256,
+    "requiresLower": 16
   },
 
   "songs": {
@@ -42,6 +46,8 @@ newGame = {
     "gainPerSec": new Decimal(0),
     "cost": 10,
     "buyCurrency": "chains",
+    "buys": 2560,
+    "requiresLower": 10,
     "generates": "money",
     "power": new Decimal(1)
   },
